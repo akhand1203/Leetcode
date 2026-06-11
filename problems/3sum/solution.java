9b9103d2bd1b35aaf1f95dbe53a0d981eac5227b
AKHAND PRATAP SINGH <123093534+akhand1203@users.noreply.github.com>
2026-06-11 09:01:09 +0000

    Sync LeetCode submission - 3Sum (java)

---
+++ problems/3sum/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int sum = -1 * nums[i];
+
+            while (left < right) {
+                if (nums[left] + nums[right] == sum) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left-1]) left++; 
+                    while (left < right && nums[right] == nums[right+1]) right--;
+                } else if (nums[left] + nums[right] < sum) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
