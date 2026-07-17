a6cc535d008d172b194f88d6a4eb697ea606f234
AKHAND PRATAP SINGH <123093534+akhand1203@users.noreply.github.com>
2026-07-17 20:44:06 +0530

    Time: 1 ms (100%), Space: 46.7 MB (30.19%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int galat=0;
+    TreeNode prev=null;
+    TreeNode g1first=null;
+    TreeNode g1second=null;
+    TreeNode g2first=null;
+    TreeNode g2second=null;
+    void fun(TreeNode root){
+        if(root==null){
+            return;
+        }
+        fun(root.left);
+        if(prev==null){
+            prev=root;
+        }
+        else{
+            if(root.val<prev.val){
+                if(galat==0){
+                    g1first=prev;
+                    g1second=root;
+                    galat++;
+                }
+                else{
+                    g2first=prev;
+                    g2second=root;
+                    galat++;
+                }
+            }
+            prev=root;
+        }
+        fun(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        fun(root);
+        if(galat==1){
+            int temp=g1first.val;
+            g1first.val=g1second.val;
+            g1second.val=temp;
+        }
+        else{
+            int temp=g1first.val;
+            g1first.val=g2second.val;
+            g2second.val=temp;
+        }
+        return;
+    }
+}
