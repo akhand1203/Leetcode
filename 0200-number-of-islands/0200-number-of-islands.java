8aed1d261c2168faeb3b22f20216b03a10a3abd1
AKHAND PRATAP SINGH <123093534+akhand1203@users.noreply.github.com>
2026-07-19 19:28:47 +0530

    Time: 9 ms (7.39%), Space: 55 MB (5.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,45 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int count = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+    boolean valid(int row, int col, int n, int m) {
+        return row >= 0 && row < n && col >= 0 && col < m;
+    }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(grid, i, j);
+    void dfs(char[][] grid, int n, int m, int row, int col, ArrayList<ArrayList<Boolean>> vis) {
+        vis.get(row).set(col, true);
+        int[] x = {-1, 1, 0, 0};
+        int[] y = {0, 0, -1, 1};
+
+        for (int k = 0; k < 4; k++) {
+            int newRow = row + x[k];
+            int newCol = col + y[k];
+            if (valid(newRow, newCol, n, m) && grid[newRow][newCol] == '1' && vis.get(newRow).get(newCol) == false) {
+                dfs(grid, n, m, newRow, newCol, vis);
+            }
+        }
+        return;
+    }
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int res = 0;
+
+        ArrayList<ArrayList<Boolean>> vis = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Boolean> t = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                t.add(false);
+            }
+            vis.add(t);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && vis.get(i).get(j) == false) {
+                    dfs(grid, n, m, i, j, vis);
+                    res++;
                 }
             }
         }
-        return count;
-    }
-
-    void dfs(char[][] grid, int i, int j) {
-        // out of bounds or water or already visited -> stop
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0';  // mark as visited (sink it)
-
-        dfs(grid, i + 1, j);  // down
-        dfs(grid, i - 1, j);  // up
-        dfs(grid, i, j + 1);  // right
-        dfs(grid, i, j - 1);  // left
+        return res;
     }
 }
