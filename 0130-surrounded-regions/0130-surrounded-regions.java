761303294d600b527f9c2dc395dfa65a9a78f766
AKHAND PRATAP SINGH <123093534+akhand1203@users.noreply.github.com>
2026-07-21 20:56:16 +0530

    Time: 1 ms (99.84%), Space: 47.7 MB (57.7%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+
+        // Step 1: mark all 'O's connected to the border as safe ('#')
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0, n, m);
+            }
+            if (board[i][m - 1] == 'O') {
+                dfs(board, i, m - 1, n, m);
+            }
+        }
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j, n, m);
+            }
+            if (board[n - 1][j] == 'O') {
+                dfs(board, n - 1, j, n, m);
+            }
+        }
+
+        // Step 2: flip remaining 'O' -> 'X' (these were surrounded), and '#' back to 'O'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    void dfs(char[][] board, int i, int j, int n, int m) {
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = '#';   // temporary marker: "safe, connected to border"
+
+        dfs(board, i + 1, j, n, m);
+        dfs(board, i - 1, j, n, m);
+        dfs(board, i, j + 1, n, m);
+        dfs(board, i, j - 1, n, m);
+    }
+}
