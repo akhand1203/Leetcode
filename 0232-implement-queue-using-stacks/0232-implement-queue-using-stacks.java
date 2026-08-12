3d66ccd065ee8f165ab19ee294e8e9610520363e
AKHAND PRATAP SINGH <123093534+akhand1203@users.noreply.github.com>
2026-08-12 18:47:00 +0530

    Time: 0 ms (100%), Space: 42.7 MB (70.42%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,36 @@
+class MyQueue {
+
+    private Deque<Integer> inStack;
+    private Deque<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new ArrayDeque<>();
+        outStack = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        transfer();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        transfer();
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void transfer() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+}
